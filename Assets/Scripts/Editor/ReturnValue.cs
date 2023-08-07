2bb8719988fc04b79da3b6fdf6545f7b38f87105
Kota Shidara <3t.u1.kota@gmail.com>
2023-08-08 00:03:38 +0900

    変更

--- Assets/Scripts/Editor/ReturnValue.cs
+++ Assets/Scripts/Editor/ReturnValue.cs
@@ -1,11 +1,28 @@
-using UnityEngine;
+using System.IO;
+using System.Text;
+using UnityEditor;
 
 public class ReturnValue
 {
-    private static string ReturnValueMethod()
+    private static void WriteStringsToFile(string[] data, string filePath)
     {
-        Debug.Log("ReturnValueMethodが呼ばれた");
-        Debug.Log("echo \"test=hello\" >> $GITHUB_OUTPUT");
-        return "valuevaluevalue";
+        StringBuilder sb = new StringBuilder();
+        foreach (var line in data)
+        {
+            sb.AppendLine(line);
+        }
+        File.WriteAllText(filePath, sb.ToString());
+    }
+
+    [MenuItem("Tools/Test")]
+    public static void CreateOutput()
+    {
+        string[] outputs = new string[]
+        {
+            "VARIABLE1=value1",
+            "VARIABLE2=value2",
+            "VARIABLE3=value3"
+        };
+        WriteStringsToFile(outputs, "Build/test.txt");
     }
 }
