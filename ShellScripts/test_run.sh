a3a3383c4536b806272119d9a3546648a58656c1
Kota Shidara <3t.u1.kota@gmail.com>
2023-08-08 21:48:47 +0900

    c

--- ShellScripts/test_run.sh
+++ ShellScripts/test_run.sh
@@ -4,4 +4,5 @@ text="$1"
 
 touch "/Users/kota/Desktop/tst.txt" 
 echo "This is a test. ${text} " > "/Users/kota/Desktop/tst.txt"
+echo "This is a test!!!!!!. ${text}"
 touch "/Users/kota/Desktop/tst${text}.txt"
