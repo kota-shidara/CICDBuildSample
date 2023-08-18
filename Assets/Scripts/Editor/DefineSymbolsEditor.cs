ac584da8ade497dde98d09b5ee7e6cc7a6c0ab19
Kota Shidara <3t.u1.kota@gmail.com>
2023-08-19 00:03:46 +0900

    c

--- Assets/Scripts/Editor/DefineSymbolsEditor.cs
+++ Assets/Scripts/Editor/DefineSymbolsEditor.cs
@@ -1,41 +1,37 @@
 using UnityEditor;
-using UnityEngine;
 
-namespace Flamers.Editor.Tools
+internal class DefineSymbolsEditor : EditorWindow
 {
-    internal class DefineSymbolsEditor : EditorWindow
+    private const string PICO_PLATFORM_DEFINE = "PICO_PLATFORM";
+
+    public void SetPicoOn()
     {
-        private const string PICO_PLATFORM_DEFINE = "PICO_PLATFORM";
+        SetDefineSymbols(true);
+    }
 
-        public void SetPicoOn()
+    public void SetPicoOff()
+    {
+        SetDefineSymbols(false);
+    }
+
+    private void SetDefineSymbols(bool isPicoOn)
+    {
+        var symbols =
+            PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+
+        if (isPicoOn)
         {
-            SetDefineSymbols(true);
-        }
-
-        public void SetPicoOff()
-        {
-            SetDefineSymbols(false);
-        }
-
-        private void SetDefineSymbols(bool isPicoOn)
-        {
-            var symbols =
-                PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
-
-            if (isPicoOn)
+            if (!symbols.Contains(PICO_PLATFORM_DEFINE))
             {
-                if (!symbols.Contains(PICO_PLATFORM_DEFINE))
-                {
-                    symbols += $";{PICO_PLATFORM_DEFINE}";
-                }
+                symbols += $";{PICO_PLATFORM_DEFINE}";
             }
-            else
-            {
-                symbols = symbols.Replace($"{PICO_PLATFORM_DEFINE};", "")
-                    .Replace(PICO_PLATFORM_DEFINE, "");
-            }
-
-            PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, symbols);
         }
+        else
+        {
+            symbols = symbols.Replace($"{PICO_PLATFORM_DEFINE};", "")
+                .Replace(PICO_PLATFORM_DEFINE, "");
+        }
+
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, symbols);
     }
 }
